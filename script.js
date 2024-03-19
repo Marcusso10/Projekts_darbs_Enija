cdb87fae5bab985e5319d88fe9e0e438b10b7e68
Marcusso10 <131649181+Marcusso10@users.noreply.github.com>
2024-03-19 14:24:28 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,93 +1,38 @@
-let poguSkaits = document.querySelectorAll(".drum").length
+document.getElementById('image-upload').addEventListener('change', function() {
+    const file = this.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function() {
+        const img = new Image();
+        img.src = reader.result;
+        img.onload = function() {
+          const color = analyzeColor(img);
+          displayColor(color);
+        }
+      }
+      reader.readAsDataURL(file);
+    }
+  });
+  
+  function analyzeColor(img) {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const pixels = imageData.data;
 
-//uztver peles spiešanu
-for (let i=0; i<poguSkaits; i++){
-document.querySelectorAll(".drum")[i].addEventListener("click",bungas)
-}
-
-//uztver taustiņu spiešanu
-document.addEventListener("keydown",function(notikums)
-{spele(notikums.key);
-animacija(notikums.key);
-});
-
-
-
-
-function bungas(){
-    let poga = this.innerHTML;
-    spele(poga)
-    animacija(poga)
-}
-
-
-function animacija(pasreizejais){
-    document.querySelector("."+ pasreizejais).classList.add("pressed");
-
-
-    setTimeout(function() {
-        document.querySelector("."+ pasreizejais).classList.remove("pressed");
-    },100);
-}
-
-
-
-
-function spele(e){
-
-switch (e){
-    case "w":
-    const tom1 = new Audio('./sounds/crash.mp3');
-    tom1.play();
-    break;
-
-    case "a":
-    const tom2 = new Audio('./sounds/kick-bass.mp3');
-    tom2.play();
-    break;
-
-    case "s":
-        const tom3 = new Audio('./sounds/snare.mp3');
-        tom3.play();
-        break;
-
-     case "d":
-         const tom4 = new Audio('./sounds/tom-1.mp3');
-         tom4.play();
-        break;
-
-        case "j":
-        const tom5 = new Audio('./sounds/tom-2.mp3');
-        tom5.play();
-        break;
-
-        case "k":
-            const tom6 = new Audio('./sounds/tom-3.mp3');
-            tom6.play();
-            break;
-        
-        case "l":
-        const tom7 = new Audio('./sounds/tom-4.mp3');
-        tom7.play();
-        break;
-
-}
-}
-
-
-function kalkulators(a,b){
-    return a+b
-}
-console.log(kalkulators(3,42))
-
-function zeizin(a,b){
-    return a*b
-}
-console.log(zeizin(3,42))
-
-
-//kkkkkkkkkkkkkkkkkkkk
-function kings(a,b,operat){
-    return operat(a,b)
-}
-console.log(kings(4,5,zeizin))
+    const color = {
+      red: pixels[0],
+      green: pixels[1],
+      blue: pixels[2]
+    };
+  
+    return color;
+  }
+  
+  function displayColor(color) {
+    const colorResultDiv = document.getElementById('color-result');
+    colorResultDiv.style.backgroundColor = `rgb(${color.red}, ${color.green}, ${color.blue})`;
+  }
