eb9a516fbc18ee34b81dcda1ea710a71a831f90d
Enijaaa <131648779+Enijaaa@users.noreply.github.com>
2024-04-08 13:40:05 +0300

    Update script.js

--- PROGRAMMA_ENIJA_MARKUS-DARBS/script.js
+++ PROGRAMMA_ENIJA_MARKUS-DARBS/script.js
@@ -1,30 +1,3 @@
-// let y = Math.floor(Math.random() * 10 + 1);
- 
-// let guess = 1;
-
-// document.getElementById("submitguess").onclick = function () {
-
-
-//     let x = document.getElementById("guessField").value;
-
-//     if (x == y) {
-//         alert("Malacis! Tu uzvarēji veicot "
-//             + guess + " minējumus ");
-//     }
-
-
-//     else if (x > y) {
-//         guess++;
-//         alert("Nepareizi! Skaitlis ir mazāks.");
-//     }
-//     else {
-//         guess++;
-//         alert("Nepareizi! Skaitlis ir lielāks.")
-//     }
-// }
-
-
-
 
 function calculateProteinIntake() {
     var mass = parseFloat(document.getElementById('mass').value);
