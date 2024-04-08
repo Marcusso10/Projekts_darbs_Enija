8549490e1621e4a96d27e95a3a3bf7c6fb55826d
Enijaaa <131648779+Enijaaa@users.noreply.github.com>
2024-04-08 20:20:42 +0300

    Update script.js

--- PROGRAMMA_ENIJA_MARKUS-DARBS/script.js
+++ PROGRAMMA_ENIJA_MARKUS-DARBS/script.js
@@ -1,15 +1,23 @@
+const date = new Date();
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
 
+let currentDate = `${day}-${month}-${year}`;
+document.querySelector('.current-date').innerHTML = currentDate;
+ 
+ 
 function calculateProteinIntake() {
-    var mass = parseFloat(document.getElementById('mass').value);
-    var age = parseInt(document.getElementById('age').value);
-    
-
-    let proteinIntake;
-    if (age >= 18) {
-       proteinIntake = mass * 1.4; 
-    } else {
-       proteinIntake = mass * 0.9; 
-    }
-    
-    document.getElementById('result').innerText = "Nepieciešamais olbaltumvielu daudzums dienā: " + proteinIntake.toFixed(2) + " grami";
-  }
+   var mass = parseFloat(document.getElementById('mass').value);
+   var age = parseInt(document.getElementById('age').value);
+   
+ 
+   let proteinIntake;
+   if (age >= 18) {
+      proteinIntake = mass * 1.4;
+   } else {
+      proteinIntake = mass * 0.9;
+   }
+   
+   document.getElementById('result').innerText = "Nepieciešamais olbaltumvielu daudzums dienā: " + proteinIntake.toFixed(2) + " grami";
+ }
